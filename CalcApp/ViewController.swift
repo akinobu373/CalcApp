03a14079b61ea429dd44582a9c2f488cb1f556f6
akinobu <siguiente.football.373@gmail.com>
2021-11-08 18:34:09 +0900

    Initial Commit

---
+++ CalcApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalcApp
+//
+//  Created by 美並　明伸 on 2021/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
