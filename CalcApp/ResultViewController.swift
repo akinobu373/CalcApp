ffec8ca66030d4a62ff8e8dc0a30e1d01d5d29d5
akinobu <siguiente.football.373@gmail.com>
2021-11-09 15:16:40 +0900

    課題が完成しました。

---
+++ CalcApp/ResultViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ResultViewController.swift
+//  CalcApp
+//
+//  Created by 美並　明伸 on 2021/11/09.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var totalView: UILabel!
+    var resultArray:[Double] = []
+       var sum:Double = 0
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+                resultArray = UserDefaults.standard.object(forKey: "item") as! [Double]
+                print(resultArray)
+                sum = resultArray.reduce(0) { (num1:Double, num2:Double) -> Double in
+                    return num1 + num2
+                }
+
+                totalView.text = String(format: "%.0f", sum)
+
+            }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CalcApp/ViewController.swift
+++ CalcApp/ViewController.swift
@@ -7,13 +7,82 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource{
+    
+    var cost:Double = 0
+    var addTaxCost:Double = 0
+    var costsArray:[Double] = []
+    var addTaxCostString:String = ""
+    
+    @IBOutlet weak var resultNum: UILabel!
+    @IBOutlet weak var itemTableView: UITableView!
+    @IBOutlet weak var taxController: UISegmentedControl!
+    @IBOutlet weak var costField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        itemTableView.delegate = self
+        itemTableView.dataSource = self
+        
+        costsArray.removeAll()
     }
-
-
+    
+    @IBAction func taxButton(_ sender: Any) {
+        if taxController.selectedSegmentIndex == 0 {
+            calc(tax: 1.1)
+        } else {
+            calc(tax: 1.08)
+        }
+    }
+    
+    @IBAction func addButton(_ sender: Any) {
+        costsArray.append(contentsOf: [addTaxCost])
+        print(costsArray)
+        UserDefaults.standard.set(costsArray, forKey: "item")
+        
+        costField.text = ""
+        resultNum.text = ""
+        
+        self.itemTableView.reloadData()
+    }
+    
+    func calc(tax:Double) {
+        cost = Double(costField.text!)!
+        addTaxCost = cost * Double(tax)
+        print(addTaxCost)
+        
+        addTaxCostString = String(format: "%.0f", addTaxCost)
+        resultNum.text = addTaxCostString
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return costsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = itemTableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        
+        cell.textLabel?.text = "\(costsArray[indexPath.row])"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            costsArray.remove(at: indexPath.row)
+            
+            UserDefaults.standard.set(costsArray, forKey: "item")
+            
+            itemTableView.reloadData()
+        }
+    }
+    
 }
 
